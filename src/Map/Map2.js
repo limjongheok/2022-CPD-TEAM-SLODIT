0a807072376b909353429469379cc0d7103ae10f
limjongheok <jjongbbang2@naver.com>
2022-05-22 17:37:18 +0900

    각 좌표에 마커

--- src/Map/Map2.js
+++ src/Map/Map2.js
@@ -1,12 +1,14 @@
 import {collection, doc, getDocs} from 'firebase/firestore';
 import {db} from '../firebase'
 import React, {useEffect ,useState} from "react";
+import Mapprops from './Mapprops';
 const { kakao } = window;
 
 
 const Map2 =() =>{
 
   const [coords, setCoords]=useState([]);
+  
   const userCollectionRef = collection(db,"users");
 
 
@@ -17,7 +19,8 @@ const Map2 =() =>{
       
   }
   
-  
+   var lats = null;
+   var lons = null;
   useEffect(()=>{
     //처음 map 그리기 
     // id 정의 및 div id 로  그리기 
@@ -90,9 +93,31 @@ const Map2 =() =>{
           });
           marker.setMap(map);
           console.log("setup 완료 ")
-          
+
+          //클릭 이벤트
+          kakao.maps.event.addListener(marker,'click', () =>{
+            console.log("클릭시 lat lon")
+            console.log(a.lat)
+            console.log(a.lon)
+            lats = a.lat;
+            lons= a.lon;
+            if(lats != null && lons !=null){
+              var iwContent = '<div style="padding:5px;">길찾기<br><a herf></a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+              iwPosition = new kakao.maps.LatLng(lats, lons), //인포윈도우 표시 위치입니다
+              iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+    
+              // 인포윈도우를 생성하고 지도에 표시합니다
+              var infowindow = new kakao.maps.InfoWindow({
+              map: map, // 인포윈도우가 표시될 지도
+              position : iwPosition, 
+              content : iwContent,
+              removable : iwRemoveable
+              });
+    
+            }
+            
+          })
         }
-        
       }
   })
 
@@ -108,10 +133,10 @@ const Map2 =() =>{
      
     
       return (
-        <>
+        <div>
           <div id="map" style={{width:'100vw' , height : 
-        '100vh'}}></div>
-        </>
+        '100vh', zIndex: 1}}></div>
+        </div>
       );
 }
 export default Map2;
--- src/Map/Mapprops.js
+++ src/Map/Mapprops.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from "react";
-const { kakao } = window;
+
 function Mapprops(lat,lon){
 
 
-    
 
 
     return(
-        <div><h1>{lat}</h1>
-        <h1>{lon}</h1></div>
+        <div style={{width :'50hw', height : '50hw'}}>
+            <h2>거리</h2>
+        </div>
         
     )
 
