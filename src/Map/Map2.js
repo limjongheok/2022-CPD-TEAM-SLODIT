e07f2793410c45f3125386a0563609f6defb7520
limjongheok <42922673+limjongheok@users.noreply.github.com>
2022-05-27 10:58:45 +0900

    내위치 마커 수정

--- src/Map/Map2.js
+++ src/Map/Map2.js
@@ -86,22 +86,23 @@ const Map2 =() =>{
               var lon = position.coords.longitude;
               centerlat =lat;
               centerlon = lon;
-             
+
+              // 내 위치 이미지 마커 
+              var imageSrc = require('./slowditmyloc.png')
+              var imageSize = new kakao.maps.Size(30, 30); // 마커이미지의 크기입니다
+              var imageOption = {offset: new kakao.maps.Point(27, 69)}; 
+              var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+              
               var locPosition = new kakao.maps.LatLng(lat,lon);
-              var message ='<div style="padding:5px">내위치</div>';
+        
               var marker = new kakao.maps.Marker({
                 map: map,
-                position: locPosition
+                position: locPosition,
+                image: markerImage
             });
-            var iwContent = message;
-            var iwRemoveable = true;
+           
+            
     
-            var infowindow = new kakao.maps.InfoWindow({
-                content: iwContent,
-                removable: iwRemoveable
-            });
-    
-            infowindow.open(map,marker)
             map.setCenter(locPosition)
           })
       }else{
