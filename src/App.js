d688e605a5e3f4d12dc19389776ac9c7943c397b
limjongheok <42922673+limjongheok@users.noreply.github.com>
2022-05-29 02:08:03 +0900

    splash page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from "react";
 import Map from "./Map/Map2"
+import Splash from "./splash/Splash";
 
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <Map/>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    isLoading : true,
+  };
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({isLoading: false});
+    },3000)
+  }
+  render(){
+    const {isLoading} = this.state;
+    return(
+      <div>
+        {isLoading ? <Splash/> : <Map/>}
+      </div>
+    )
+
+  }
+    
 }
 
 export default App;
---
+++ src/splash/Splash.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Splash.module.css'
+
+const Splash= () =>{
+
+  return(
+    <div>
+      <div className={styles.splashimg}><p className={styles.splashfont}>S L O W D I T</p></div>
+    </div>
+  )
+}
+export default Splash
