b287797d99a71d833b88a845615397c33ebb5dea
lim-jongheok <20180719gm.hannam.ac.kr>
2022-06-01 23:23:27 +0900

    로고 변경

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () =>{
         <div>
             <div className={styles.Header_main}>
                 <div className={styles.Header_flexbox}>
-                <div className={styles.Header_font} >S L O W D I T</div>
+                <div className={styles.Header_font} >F I N D I T</div>
                 <img  src={require('./slowditiconwhite.png')} alt="log" className={styles.Header_logo}></img>
                 </div> 
                 
